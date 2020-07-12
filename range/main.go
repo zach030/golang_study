5af897e1261e28c278f3115039c21fccf0ad9cca
Zach <zachchou016@gmail.com>
2020-07-12 09:24:32 +0800

    [update] uodate main-func code

--- range/main.go
+++ range/main.go
@@ -32,16 +32,6 @@ func main() {
 		}
 	}
 
-	// `range` on map iterates over key/value pairs.
-	kvs := map[string]string{"a": "apple", "b": "banana"}
-	for k, v := range kvs {
-		fmt.Printf("%s -> %s\n", k, v)
-	}
-
-	// `range` can also iterate over just the keys of a map.
-	for k := range kvs {
-		fmt.Println("key:", k)
-	}
 
 	// `range` on strings iterates over Unicode code
 	// points. The first value is the starting byte index
